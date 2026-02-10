212afe32dd378d98ed327794b112042176c5b0b9
Hadi sedaghat <co.eng.uof20@gmail.com>
2026-02-10 18:25:37 +0330

    Creating a list of books, choosing requested books, and show 5 parts.

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-response_situation = requests.get("https://openlibrary.org/search.json?q=python&limit=50")
-print("situation: " + str(response_situation.status_code))
+library_url ="https://openlibrary.org/search.json?q=python&limit=50"
+response_situation = requests.get(library_url)
 
+print("status: " + str(response_situation.status_code))
 data = response_situation.json()
-books_info = data['docs'][:50]
+books_info = data['docs']
 
-books = [b for b in books_info if 'first_publish_year' in b and b['first_publish_year'] > 2000]
-for result in books:
+books_over_2000 = [book for book in books_info if 'first_publish_year' in book and book['first_publish_year'] > 2000]
+for result in books_over_2000:
     print(result)
+
+requested_books = []
+for book in books_over_2000:
+    requested_books.append({
+        "title": book.get('title'),
+        "language": ",".join(book.get('language', [])),
+        "publish_year": book.get('first_publish_year'),
+        "ebook_access": book.get('ebook_access'),
+        "author": ",".join(book.get('author_name', [])),
+    })
+
+for book in requested_books:
+    print(book["title"], book["language"], book["publish_year"], book["ebook_access"], book["author"])
