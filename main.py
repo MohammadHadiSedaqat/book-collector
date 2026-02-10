68305bf567fb37f7a0d4d92169dddf903adb19bb
Hadi sedaghat <co.eng.uof20@gmail.com>
2026-02-10 19:29:57 +0330

    Creating csv file included requested books.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
-from bs4 import BeautifulSoup
+import csv
 
-library_url ="https://openlibrary.org/search.json?q=python&limit=50"
+library_url ="https://openlibrary.org/search.json?q=python&limit=58"
 response_situation = requests.get(library_url)
 
 print("status: " + str(response_situation.status_code))
@@ -9,8 +9,8 @@ data = response_situation.json()
 books_info = data['docs']
 
 books_over_2000 = [book for book in books_info if 'first_publish_year' in book and book['first_publish_year'] > 2000]
-for result in books_over_2000:
-    print(result)
+# for result in books_over_2000:
+#     print(result)
 
 requested_books = []
 for book in books_over_2000:
@@ -22,5 +22,12 @@ for book in books_over_2000:
         "author": ",".join(book.get('author_name', [])),
     })
 
-for book in requested_books:
-    print(book["title"], book["language"], book["publish_year"], book["ebook_access"], book["author"])
+# for book in requested_books:
+#     print(book["title"], book["language"], book["publish_year"], book["ebook_access"], book["author"])
+
+keys = ["title", "language", "publish_year", "ebook_access", "author"]
+
+with open('books.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=keys)
+    writer.writeheader()
+    writer.writerows(requested_books)
